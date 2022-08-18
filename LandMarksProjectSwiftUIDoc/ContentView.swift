effe6409755c733e1017d96a5ce6093579c50cc1
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-08-18 14:17:47 +0300

    Initial Commit

---
+++ LandMarksProjectSwiftUIDoc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMarksProjectSwiftUIDoc
+//
+//  Created by Ömer Faruk Kılıçaslan on 18.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LandMarksProjectSwiftUIDoc/LandMarksProjectSwiftUIDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksProjectSwiftUIDocApp.swift
+//  LandMarksProjectSwiftUIDoc
+//
+//  Created by Ömer Faruk Kılıçaslan on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksProjectSwiftUIDocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
